3c50fcae270d9aa6d3e6bf1b5253999a9af9f9e0
eimera <marie.andre_a@yahoo.com>
2026-05-20 14:33:02 +0200

    Ajout iDAO

--- src/main/java/quack/dao/IDAO.java
+++ src/main/java/quack/dao/IDAO.java
@@ -1,5 +1,13 @@
 package quack.dao;
 
-public interface IDAO {
+import java.util.List;
 
+public interface IDAO <T, K>{
+
+	T findById(int id);
+	List<T> findAll();
+	boolean save(T t);
+	boolean update(T t);
+	boolean delete(int id);
 }
+
