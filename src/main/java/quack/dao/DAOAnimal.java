ac2cfd16df769441f3dae00f11f98cf02fa133df
eimera <marie.andre_a@yahoo.com>
2026-05-20 14:54:21 +0200

    Modif DAO Animal et IDAO Animal

--- src/main/java/quack/dao/DAOAnimal.java
+++ src/main/java/quack/dao/DAOAnimal.java
@@ -1,5 +1,54 @@
 package quack.dao;
 
+import java.util.List;
+
+import jakarta.persistence.EntityManager;
+import quack.model.Animal;
+import quack.model.Lieu;
+
 public class DAOAnimal implements IDAOAnimal {
 
+
+	private EntityManager em;
+
+	public void AnimalDAO(EntityManager em) {
+		this.em = em;
+	}
+
+	@Override
+	public Animal findById(Integer id) {
+		return em.find(Animal.class, id);
+	}
+
+	@Override
+	public List<Animal> findAll() {
+		return em.createQuery("SELECT a FROM Animal a", Animal.class)
+				.getResultList();
+	}
+
+	@Override
+	public Animal save(Animal animal) {
+		em.getTransaction().begin();
+		em.persist(animal);
+		em.getTransaction().commit();
+		return animal;
+	}
+
+	@Override
+	public Animal update(Animal animal) {
+		em.getTransaction().begin();
+		Animal updated = em.merge(animal);
+		em.getTransaction().commit();
+		return updated;
+	}
+
+	@Override
+	public void delete(Integer id) {
+		em.getTransaction().begin();
+		Animal animal = em.find(Animal.class, id);
+		if (animal != null) em.remove(animal);
+		em.getTransaction().commit();
+	}
 }
+
+
--- src/main/java/quack/dao/IDAOAnimal.java
+++ src/main/java/quack/dao/IDAOAnimal.java
@@ -1,5 +1,20 @@
 package quack.dao;
 
+import java.util.List;
+
+import quack.model.Animal;
+
 public interface IDAOAnimal {
 
+	public Animal findById(Integer id);
+	
+	public List<Animal> findAll();
+	
+	public Animal save(Animal animal); 
+
+	public Animal update(Animal animal);
+
+	public void delete(Integer id);
+
+	
 }
