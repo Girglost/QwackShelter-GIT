b42f9b438a0b930b2c9259f851b4e98f60c18f02
Hestia <clea.vcd@gmail.com>
2026-05-25 19:48:10 +0200

    Modif IDAOAnimal -> ajout de méthode

--- src/main/java/quack/dao/IDAOAnimal.java
+++ src/main/java/quack/dao/IDAOAnimal.java
@@ -3,12 +3,17 @@ package quack.dao;
 import java.util.List;
 
 import quack.model.Animal;
+import quack.model.Genre;
 
 public interface IDAOAnimal extends IDAO<Animal,Integer> {
 
 	public Animal update(Animal animal);
 
-public Animal findByName(Animal String);
+	public Animal findByName(String name);
 
-	
+	public List<Animal> findByGenre(Genre genre);
+
+	public List<Animal> findByType(String type);
+
+	public List<Animal>	findByDispo();
 }
