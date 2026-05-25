4ed4037b8c14c3fa600dc62f4620027a78352bec
Hestia <clea.vcd@gmail.com>
2026-05-25 18:48:29 +0200

    Mamifère + oiseaux

---
+++ src/main/java/quack/model/Oiseaux.java
@@ -0,0 +1,67 @@
+package quack.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("OISEAU")
+public class Oiseaux extends Animal {
+          
+    protected boolean capaciteVol;  
+    protected boolean pondeuse;
+    
+    
+	public Oiseaux(int id, String nomAnimal, LocalDate dateNaissance, String couleur, String regimeAlimentaire,
+			String traitement, Famille famille, Genre genre, List<Caractere> caracteres, QuackShelter qwackShelter,
+			boolean capaciteVol, boolean pondeuse) {
+		super(id, nomAnimal, dateNaissance, couleur, regimeAlimentaire, traitement, famille, genre, caracteres,
+				qwackShelter);
+		this.capaciteVol = capaciteVol;
+		this.pondeuse = pondeuse;
+	}
+
+
+	public Oiseaux() {
+
+	}
+
+
+	public boolean isCapaciteVol() {
+		return capaciteVol;
+	}
+
+
+	public void setCapaciteVol(boolean capaciteVol) {
+		this.capaciteVol = capaciteVol;
+	}
+
+
+	public boolean isPondeuse() {
+		return pondeuse;
+	}
+
+
+	public void setPondeuse(boolean pondeuse) {
+		this.pondeuse = pondeuse;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Oiseaux [id=" + id + ", nomAnimal=" + nomAnimal + ", dateNaissance=" + dateNaissance + ", couleur="
+				+ couleur + ", regimeAlimentaire=" + regimeAlimentaire + ", traitement=" + traitement + ", famille="
+				+ famille + ", genre=" + genre + ", caracteres=" + caracteres + ", qwackShelter=" + qwackShelter
+				+ ", historiqueSante=" + historiqueSante + ", statutAnimal=" + statutAnimal + ", capaciteVol="
+				+ capaciteVol + ", pondeuse=" + pondeuse + "]";
+	}
+	
+	
+	
+	
+    
+    
+
+}
