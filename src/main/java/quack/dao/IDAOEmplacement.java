eec7ece4f206b06853a0eb94445918ca89b5f9a7
GILET Ronan <ronan.gilet@gmail.com>
2026-05-27 10:34:05 +0200

    DAO/IDAO Emplacement
    Clean IDAO + Modif CRUD DAO

--- src/main/java/quack/dao/IDAOEmplacement.java
+++ src/main/java/quack/dao/IDAOEmplacement.java
@@ -7,14 +7,5 @@ import quack.model.Emplacement;
 
 public interface IDAOEmplacement extends IDAO<Emplacement,Integer> {
 
-	public Emplacement findById(Integer id);
-
-	public List<Emplacement> findAll();
-
-	public Emplacement save(Emplacement emplacement); 
-
-	public Emplacement update(Emplacement emplacement);
-
-	public void deleteById(Integer id);
 
 }
