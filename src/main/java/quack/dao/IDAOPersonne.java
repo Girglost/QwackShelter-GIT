07b431b88c81b77eab6d56f7ee69a28ef726c1da
YohannLG01 <yohannleguen@hotmail.com>
2026-06-05 16:58:32 +0200

    maj IDAOPersonne spring

--- src/main/java/quack/dao/IDAOPersonne.java
+++ src/main/java/quack/dao/IDAOPersonne.java
@@ -3,6 +3,7 @@ package quack.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import quack.model.Benevole;
 import quack.model.Employe;
@@ -11,13 +12,13 @@ import quack.model.Personne;
 import quack.model.Visiteur;
 
 public interface IDAOPersonne extends JpaRepository<Personne,Integer>{
-
+	@Query("FROM Patron")
 	public List<Patron> findAllPatron();
-	
+	@Query("FROM Visiteur")
 	public List<Visiteur> findAllVisiteur();
-	
+	@Query("FROM Employe")
 	public List<Employe> findAllEmploye();
-	
+	@Query("FROM Benevole")
 	public List<Benevole> findAllBenevole();
 	
 	public Personne findByLoginAndPassword(String login,String password);
