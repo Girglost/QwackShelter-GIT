95b68b3ecdd7ac074512ff12f190c0d9b8ef2f1c
Cléa <clea.vcd@gmail.com>
2026-07-02 20:26:35 +0200

    QwackShelterRC

---
+++ qwack-boot/src/main/java/qwack_boot/restcontroller/QuackShelterRC.java
@@ -0,0 +1,49 @@
+package qwack_boot.restcontroller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import qwack_boot.model.QuackShelter;
+import qwack_boot.service.QuackShelterService;
+
+public class QuackShelterRC {
+
+     @Autowired
+    QuackShelterService QuackShelterSrv;
+
+    @GetMapping
+    public List<QuackShelter> chercherTous() {
+        return QuackShelterSrv.getAll();
+    }
+    
+    @GetMapping("/{id}")
+    public QuackShelter chercherParId(@RequestParam Integer id) {
+        return QuackShelterSrv.getById(id);
+    }
+    
+    @DeleteMapping("/{id}")
+    public void supprimer(@PathVariable Integer id){
+        QuackShelterSrv.delete(id);
+    }
+
+    @PostMapping
+    public void ajouter(@RequestBody QuackShelter emp) {
+        QuackShelterSrv.insert(emp);
+    }
+
+    @PutMapping("/{id}")
+    public void modifier(@PathVariable Integer id, @RequestBody QuackShelter emp) {
+        emp.setId(id);
+        QuackShelterSrv.update(emp);
+    }
+    
+    
+}
