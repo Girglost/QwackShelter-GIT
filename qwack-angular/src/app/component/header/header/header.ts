06fa79ce009d6a30bc0160889dc716b01b60b1d8
GILET Ronan <ronan.gilet@gmail.com>
2026-07-08 16:54:11 +0200

    Header Fini sauf CSS

--- qwack-angular/src/app/component/header/header/header.ts
+++ qwack-angular/src/app/component/header/header/header.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { Blazon } from '../blazon/blazon';
+import { Bouttons } from '../bouttons/bouttons';
+import { NavigationHeader } from '../navigation-header/navigation-header';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    NavigationHeader,
+    Blazon,
+    Bouttons,
+    RouterLink
+  ],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
