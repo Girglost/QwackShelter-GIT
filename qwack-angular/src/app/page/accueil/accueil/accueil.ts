1f7f4309cf8b715986a1062c56ddbfdf8c2bde49
eimera <marie.andre_a@yahoo.com>
2026-07-09 11:39:38 +0200

    suppr des imports inutiles

--- qwack-angular/src/app/page/accueil/accueil/accueil.ts
+++ qwack-angular/src/app/page/accueil/accueil/accueil.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
 import { Footer } from '../../../component/footer/footer/footer';
 import { Header } from '../../../component/header/header/header';
 
 @Component({
   selector: 'app-accueil',
-  imports: [RouterLink,RouterOutlet,Footer,Header],
+  imports: [Footer,Header],
   templateUrl: './accueil.html',
   styleUrl: './accueil.css',
 })
