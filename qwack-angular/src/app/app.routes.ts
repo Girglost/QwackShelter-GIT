66b01a864869c9498ad1d8001651d3517a074a18
Hestia <clea.vcd@gmail.com>
2026-07-09 16:18:28 +0200

    route

--- qwack-angular/src/app/app.routes.ts
+++ qwack-angular/src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { Footer } from './component/footer/footer/footer';
-import { Header } from './component/header/header/header';
 import { Accueil } from './page/accueil/accueil/accueil';
 import { Actualites } from './page/actualites/actualites';
 import { Contact } from './page/contact/contact';
@@ -8,13 +6,13 @@ import { EmplacementPage } from './page/emplacement-page/emplacement-page';
 import { NousRejoindre } from './page/nous-rejoindre/nous-rejoindre';
 import { NotFound } from './page/not-found/not-found';
 
-
 export const routes: Routes = [
-  {path : "accueil", component: Accueil},
-  {path : "actualites", component: Actualites},
-  {path : "contact", component: Contact},
-  {path : "nous-rejoindre", component: NousRejoindre},
+  { path: 'accueil', component: Accueil },
+  { path: 'actualites', component: Actualites },
+  { path: 'contact', component: Contact },
+  { path: 'nous-rejoindre', component: NousRejoindre },
 
-  { path: '**', component: NotFound }
-  {path : "emplacement", component : EmplacementPage},
+  { path: 'emplacement', component: EmplacementPage },
+
+  { path: '**', component: NotFound }, // a mettre a la fin de toutes les routes pour gérer les pages non trouvées
 ];
