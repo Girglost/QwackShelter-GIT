ad0e553421877445e061d27ab281fbe262969d9b
GILET Ronan <ronan.gilet@gmail.com>
2026-07-09 21:12:13 +0200

    Angular
    App Route Correction conflit

--- qwack-angular/src/app/app.routes.ts
+++ qwack-angular/src/app/app.routes.ts
@@ -8,17 +8,23 @@ import { NotFound } from './page/not-found/not-found';
 import { DevenirBenevole } from './page/devenir-benevole/devenir-benevole';
 import { NosMissions } from './page/nos-missions/nos-missions';
 import { SwipeCompagnon } from './page/swipe-compagnon/swipe-compagnon';
+import { StatutAnimalPage } from './page/statut-animal-page/statut-animal-page';
+import { HistoriqueSantePage } from './page/historique-sante-page/historique-sante-page';
 
 export const routes: Routes = [
   { path: 'accueil', component: Accueil },
   { path: 'actualites', component: Actualites },
   { path: 'contact', component: Contact },
   { path: 'nous-rejoindre', component: NousRejoindre },
-
-  { path: 'emplacement', component: EmplacementPage },
   { path: 'devenir-benevole', component: DevenirBenevole },
   { path: 'nos-missions', component: NosMissions },
   { path: 'swipe-compagnon', component: SwipeCompagnon },
+
+  // ====== Routes vers les CRUD des classes ======
+  { path: 'emplacement', component: EmplacementPage },
+  { path: 'sAnimal', component: StatutAnimalPage},
+  { path: 'hSante', component: HistoriqueSantePage},
+
 
   { path: '**', component: NotFound }, // a mettre a la fin de toutes les routes pour gérer les pages non trouvées
 ];
