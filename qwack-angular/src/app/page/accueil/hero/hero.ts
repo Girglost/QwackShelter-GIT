756fa789a53718b35d31d74a4b8ef0a04c9eab07
GILET Ronan <ronan.gilet@gmail.com>
2026-07-10 14:44:24 +0200

    Angular
    Je sais pas pourquoi j'ai ca

--- qwack-angular/src/app/page/accueil/hero/hero.ts
+++ qwack-angular/src/app/page/accueil/hero/hero.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router,RouterLink } from '@angular/router';
+import {Observable } from 'rxjs/internal/Observable';
+import { Animal } from './../../../model/animal';
+import { AnimalService } from '../../../service/animal-service';
 
 @Component({
   selector: 'app-hero',
@@ -7,4 +10,45 @@ import { RouterLink } from '@angular/router';
   templateUrl: './hero.html',
   styleUrl: './hero.css',
 })
-export class Hero {}
+export class Hero implements OnInit {
+
+  private animalSrv: AnimalService = inject(AnimalService);
+  private router: Router = inject(Router);
+
+  animalDuJour: Animal | null = null;
+
+ngOnInit(): void {
+  console.log("Début chargement animal");
+
+  this.trouverAnimal().subscribe({
+    next: (animal) => {
+      console.log("Animal reçu :", animal);
+      this.animalDuJour = animal;
+    },
+    error: (err) => {
+      console.error("Erreur API :", err);
+    },
+    complete: () => {
+      console.log("Chargement terminé");
+    }
+  });
+}
+
+private trouverAnimal(): Observable<Animal> {
+  const id = 1;
+
+  const resultat = this.animalSrv.findById(id);
+
+  console.log("Observable créé :", resultat);
+
+  return resultat;
+}
+
+  voirProfil(): void {
+      alert("clic");
+        console.log("animal actuel :", this.animalDuJour);
+    if (this.animalDuJour) {
+      this.router.navigate(['/animal', this.animalDuJour.id]);
+    }
+  }
+}
