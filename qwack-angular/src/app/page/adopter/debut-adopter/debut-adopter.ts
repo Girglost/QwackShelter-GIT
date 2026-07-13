2fbc1377596555684491c014ae2daedf2d03ef4c
Cléa <clea.vcd@gmail.com>
2026-07-13 15:40:06 +0200

    modif routerlink + employe-page

--- qwack-angular/src/app/page/adopter/debut-adopter/debut-adopter.ts
+++ qwack-angular/src/app/page/adopter/debut-adopter/debut-adopter.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-debut-adopter',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './debut-adopter.html',
   styleUrl: './debut-adopter.css',
 })
--- qwack-angular/src/app/page/adopter/test-adopter/test-adopter.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-test-adopter',
-  imports: [],
-  templateUrl: './test-adopter.html',
-  styleUrl: './test-adopter.css',
-})
-export class TestAdopter {}
